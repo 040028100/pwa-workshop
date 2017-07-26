60e17123f4c4a034d9e079ce8c7026dd4ca2604e
Michael North <michael.l.north@gmail.com>
2017-07-26 18:42:08 -0500

    Exercise 7 - Fallback images via intercepting fetch

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,22 +1,46 @@
-let counts = {
-  install: 0,
-  activate: 0,
-  fetch: 0
-}
+import { ALL_CACHES, ALL_CACHES_LIST, removeUnusedCaches } from './sw/caches';
 
+const FALLBACK_IMAGE_URL = 'https://localhost:3100/images/fallback-grocery.png';
 
 self.addEventListener('install', event => {
   // Do the stuff needed at install time.
   // This may not be the active service worker yet.
-  console.log('install count: ', ++counts.install);
+  event.waitUntil(
+    caches.open(ALL_CACHES.fallbackImages).then((cache) => {
+      cache.add(FALLBACK_IMAGE_URL)
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
   // We are ready to take the stage.
-  console.log('activate count: ', ++counts.activate);
+  event.waitUntil(removeUnusedCaches(ALL_CACHES_LIST));
 });
+
+function fetchImageOrFallback(fetchEvent) {
+  return fetch(fetchEvent.request, {
+    mode: 'cors'
+  }).then((response) => {
+    if (!response.ok) {
+      return caches.match(FALLBACK_IMAGE_URL, {cacheName: ALL_CACHES.fallbackImages});
+    } else return response;
+  }).catch(() => {
+    return caches.match(FALLBACK_IMAGE_URL, {cacheName: ALL_CACHES.fallbackImages});
+  });
+}
 
 self.addEventListener('fetch', event => {
   // Intercept a network request.
-  console.log('fetch count: ', ++counts.fetch);
+
+  // Get the Acecpt header from the request
+  let acceptHeader = event.request.headers.get('accept');
+  // Build a URL object from the request's url string
+  let requestUrl = new URL(event.request.url);
+
+  if (acceptHeader.indexOf('image/*') >= 0 && // if it's an image
+    requestUrl.pathname.indexOf('/images/') === 0) { // and the url looks right
+    event.respondWith(
+      fetchImageOrFallback(event)
+    );
+  }
 });
