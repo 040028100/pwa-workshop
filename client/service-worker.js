ac9e49ba39aff7ace8795a60aaa612373f8998ee
Michael North <michael.l.north@gmail.com>
2017-07-28 01:05:29 -0500

    Exercise 8 - precache for quick boot

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,4 +1,8 @@
-import { ALL_CACHES, ALL_CACHES_LIST, removeUnusedCaches } from './sw/caches';
+import {
+  ALL_CACHES, ALL_CACHES_LIST,
+  removeUnusedCaches,
+  precacheStaticAssets
+} from './sw/caches';
 
 const FALLBACK_IMAGE_URL = 'https://localhost:3100/images/fallback-grocery.png';
 
@@ -7,7 +11,9 @@ self.addEventListener('install', event => {
   // This may not be the active service worker yet.
   event.waitUntil(
     caches.open(ALL_CACHES.fallbackImages).then((cache) => {
-      cache.add(FALLBACK_IMAGE_URL)
+      cache.add(FALLBACK_IMAGE_URL).then(() => {
+        return precacheStaticAssets();
+      })
     })
   );
 });
@@ -37,10 +43,19 @@ self.addEventListener('fetch', event => {
   // Build a URL object from the request's url string
   let requestUrl = new URL(event.request.url);
 
-  if (acceptHeader.indexOf('image/*') >= 0 && // if it's an image
-    requestUrl.pathname.indexOf('/images/') === 0) { // and the url looks right
-    event.respondWith(
-      fetchImageOrFallback(event)
-    );
-  }
+  event.respondWith(
+    caches.match(event.request, {cacheName: ALL_CACHES.prefetch})
+      .then((resp) => {
+        return resp || fetch(event.request);
+      })
+      .catch(() => {
+        console.log('NOT responeded to ' + event.request.url + ' from cache');
+        if (acceptHeader.indexOf('image/*') >= 0 && // if it's an image
+            requestUrl.pathname.indexOf('/images/') === 0) { // and the url looks right
+          return fetchImageOrFallback();
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
 });
